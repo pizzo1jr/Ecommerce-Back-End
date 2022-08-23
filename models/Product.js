12aeedf2115a1db3d70c4145429dfc31efc088fb
pizzo1jr <102200085+pizzo1jr@users.noreply.github.com>
2022-08-23 11:04:47 -0400

    Product model set up

---
+++ models/Product.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Product extends Model {}
+
+Product.init(
+    {
+        // define id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // define priduct_name column
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define price column
+        price: {
+            type: DataTypes.DECIMAL (10,2),
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
+        },
+        // define stock column
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 10,
+            validate: {
+                isNumeric: true
+            }
+        },
+        // createes category_id column
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product'
+    }
+);
+
+module.exports = Product;
