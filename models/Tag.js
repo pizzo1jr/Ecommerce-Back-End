72fe0b96e43f829f09eb5a9c87841eed0e76cb5f
pizzo1jr <102200085+pizzo1jr@users.noreply.github.com>
2022-08-23 11:10:56 -0400

    model tags complete

---
+++ models/Tag.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Tag extends Model {}
+
+Tag.init(
+    {
+        // defines the id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        // creates tag_name column
+        tag_name: {
+            type: DataTypes.STRING
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'tag'
+    }
+);
+
+module.exports = Comment;
